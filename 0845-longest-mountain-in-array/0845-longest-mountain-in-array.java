f2128b9d7672ec496eb80fc292bb95ed948abdd9
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-06-29 20:52:42 +0530

    Time: 2 ms (99.75%), Space: 47.2 MB (83.42%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+        int i = 1;
+        int longest = 0;
+        int n = arr.length;
+
+        while(i < n -1) {
+            if(arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                int left = i - 1;
+                while(left > 0 && arr[left] > arr[left - 1]) {
+                    left--;
+                }
+                int right = i + 1;
+                while(right < n -1 && arr[right] > arr[right + 1]) {
+                    right++;
+                }
+                longest = Math.max(longest, right - left + 1);
+                i = right;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return longest;
+    }
+}
