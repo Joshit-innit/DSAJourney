408fd9c73daebeb83a079ba19c7ec68b75ff862d
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-01 13:50:47 +0530

    Improve countNegatives method with comments
    Refactor countNegatives method for clarity and comments.

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -3,15 +3,18 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
-        int row = m - 1;
+        int row = m - 1; 
         int col = 0;
+
+        // Start from the bottom left corner
+        // You cannot start from the top right corner as u dont gain any information
         while (row >= 0 && col < n) {
             if (grid[row][col] < 0) {
-                count += n - col;
-                row--;
+                count += n - col; // if you see an negative number count the elements
+                row--; // as you saw the negative element go up
             }
             else {
-                col++;
+                col++; // As you didnt see the negative number, move util u find the next negative number
             }
         }
         return count;
