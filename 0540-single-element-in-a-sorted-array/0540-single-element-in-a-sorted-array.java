5ba99b815bd0c41b9f57458c535beae86ea51f1f
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-03 12:52:01 +0530

    Time: 0 ms (100%), Space: 52.9 MB (57.34%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,7 +1,27 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = 0;
-        for (int num : nums)  ans ^=num;
-        return ans;
+        // int ans = 0;
+        // for (int num : nums) {
+        //     ans = ans ^ num;  // same numbers get cancel due to XOR operator but the single digit remains at the last
+        // }
+            
+        // return ans;
+
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid % 2 != 0) mid = mid - 1;
+            if (nums[mid] == nums[mid+1]) {
+                
+                left = mid + 2;
+            }else{
+                right = mid;
+            }
+           
+        }
+
+        return nums[left];
+
     }
 }
