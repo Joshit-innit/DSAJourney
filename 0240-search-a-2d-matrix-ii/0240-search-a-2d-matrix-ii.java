75582582a70005563ff8e779899f58f2ec80278a
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-05 20:24:18 +0530

    Time: 3 ms (89.05%), Space: 48 MB (92.65%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -9,8 +9,16 @@ class Solution {
         // }
         // return false;
 
+                    
+                        
+        //                 cols
+        // rows ->[1,4,7,11,15]
+        //        [2,5,8,12,19]
+        //        [3,6,9,16,22]
+        //        [10,13,14,17,24]
+        //        [18,21,23,26,30]
 
-
+        
         
         int rows = matrix.length;
         int cols = matrix[0].length;
