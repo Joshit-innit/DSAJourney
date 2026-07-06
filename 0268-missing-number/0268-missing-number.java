4bc9d89456e8f83a806790f70b6aa458a7e17c59
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-06 11:05:30 +0530

    Time: 8 ms (16.38%), Space: 48.2 MB (5.63%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,10 +3,30 @@ class Solution {
         // 0 + 1 + 2 + 3 = 6
         // 0 + 1 + 3 = 4
         int n = nums.length;
-        int Actualsum = n * (n + 1 )/ 2;
-        int sum = 0;
-        for (int num : nums)  sum += num;
-        return Actualsum - sum;
+        // int Actualsum = n * (n + 1 )/ 2;
+        // int sum = 0;
+        // for (int num : nums)  sum += num;
+        // return Actualsum - sum;
+
+
+        Arrays.sort(nums);
+        int left = 0;
+        int right = n - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left)/ 2;
+
+            if(mid== nums[mid]) {
+                left = mid + 1;
+            }
+            else {
+                ans = mid;
+                right = mid - 1;
+            }
+        }
+
+        return (ans == - 1)?n:ans;
 
     }
 }
