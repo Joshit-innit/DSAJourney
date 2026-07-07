6b0fd092e3e31a88bb6de0b6256223a0fc7a3e51
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-07 11:20:57 +0530

    Time: 1 ms (97.46%), Space: 80.1 MB (91.08%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -16,6 +16,9 @@ class Solution {
         int m = nums2.length;
         int i = 0;
         int j = 0;
+        if(nums1[0] > nums2[m-1] || nums1[n-1] < nums2[0]) {
+            return -1;
+        } 
         while(i < n && j < m) {
             if (nums1[i] == nums2[j]) {
                 return nums1[i];
