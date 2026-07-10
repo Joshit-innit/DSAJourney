cc6c7623787fe21eb1855a39b23166c332a0f690
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-10 14:40:37 +0530

    Enhance comments for clarity in binary search logic
    Added comments to clarify the logic for finding the minimum in a rotated sorted array.

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -9,10 +9,12 @@ class Solution {
             int mid = left + (right - left) / 2;
 
             if (nums[left] <= nums[mid]) {
+                // the left half is sorted in ascending order, so pick the left most element and find the minimum and move towards right
                 ans = Math.min(ans, nums[left]);
                 left = mid + 1;
             }
             else {
+                // the right half is sorted in ascending order, so pick the first element which is nums[mid] and move towards left
                 ans = Math.min(ans, nums[mid]);
                 right = mid - 1;
             }
