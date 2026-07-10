dd5dd3e6fd4b1ff2721ba8ed3155c6852fb4243e
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-10 12:33:58 +0530

    Time: 0 ms (100%), Space: 43.7 MB (81.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,23 +1,9 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int ans = 0;
-        int left = 0;
         int n = nums.length;
+
+        int left = 0;
         int right = n - 1;
-
-        // while(left <= right) {
-        //     int mid = left + (right - left) / 2;
-
-        //     if (nums[mid] == target) {
-        //         return mid;
-        //     }else if (mid < n - 1 && nums[mid + 1] < target) {
-        //         left = mid + 1;
-        //     }else if (mid > 0 && nums[mid - 1] < target){
-        //         right = mid - 1;
-        //     }
-        // }
-
-        // return -1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
@@ -26,25 +12,27 @@ class Solution {
                 return mid;
             }
 
-            // Left half is sorted
             if (nums[left] <= nums[mid]) {
-
+                // left Sorted 
                 if (nums[left] <= target && target < nums[mid]) {
+                    // If the the target is in this range nums[left]<=target<nums[mid]. We dont need to check the right so we move towards the left part;
                     right = mid - 1;
-                } else {
+                }
+                else {
+                    // if the target is not in the range of nums[left]<=target<nums[mid]. We dont need to check the left so we move towards the right part;
                     left = mid + 1;
                 }
-
             }
-            // Right half is sorted
             else {
-
+                // Right sorted part
                 if (nums[mid] < target && target <= nums[right]) {
+                    // if the target is in this range nums[mid]<target<=nums[right]. We dont need to check the left part so we the right part;
                     left = mid + 1;
-                } else {
+                }
+                else {
+                    // if the target is not in the range num[mid]<target<=nums[right]. We dont nee to check the right part so we move towards left;
                     right = mid - 1;
                 }
-
             }
         }
 
