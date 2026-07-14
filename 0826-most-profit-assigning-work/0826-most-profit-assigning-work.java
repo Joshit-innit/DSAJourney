6bc75514f665d404fb708b145d93ce4e6c7aa447
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-14 18:29:54 +0530

    Enhance code readability with comments
    Added comments to clarify the purpose of code sections.

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -3,17 +3,19 @@ class Solution {
         int n = profit.length;
         int jobs[][] = new int[n][2];
         int total = 0;
-
+        // create a new array of difficulty and profits
         for (int i = 0; i < n; i++) {
             jobs[i][0] = difficulty[i];
             jobs[i][1] = profit[i];
         }
-
+        
+        // Sort them according to difficulty, because we need to search each worker according to their difficulty level
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-
+        // Build a prefix maximum, so that if lower difficulty work which as higher profits is not missed
         for (int i = 1; i < n; i++) {
             jobs[i][1] = Math.max(jobs[i][1], jobs[i-1][1]);
         }
+        // Do a upper bound, find a point until the worker difficulty level is satisfied in the jobs difficulty
         for (int target : worker) {
             int left = 0;
             int right = n - 1;
