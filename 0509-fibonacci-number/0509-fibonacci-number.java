9f4ea3f840ab0dca2ac1c2c454eae6c54d1ab96a
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-07-21 22:20:05 +0530

    Time: 8 ms (45.13%), Space: 42.1 MB (33.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,5 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
-        }else if (n == 1) {
-            return 1;
-        }
-        return fib(n - 1) + fib(n - 2);
+        return (n <= 1)?n:fib(n-1) + fib(n-2);
     }
 }
