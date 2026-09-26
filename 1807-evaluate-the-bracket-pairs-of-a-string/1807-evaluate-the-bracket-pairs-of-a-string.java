91bb974602c56cb13b3ce3ca2186a99239c8c1a9
TAMMANA JOSHIT <2400032515@kluniversity.in>
2026-09-26 19:25:07 +0530

    Time: 34 ms (79.64%), Space: 90.9 MB (71.04%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -4,25 +4,24 @@ class Solution {
         for (List<String> list : knowledge) {
             map.put(list.get(0), list.get(1));
         }
-
-        int start = -1;
         StringBuilder result = new StringBuilder();
         int n = s.length();
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-
             if (ch == '(') {
-                start = i;
-            }
-            else if (ch == ')') {
-                String value = s.substring(start + 1, i);
+                int j = i + 1;
+                while (s.charAt(j) != ')') {
+                    j++;
+                }
+                result.append(map.getOrDefault(s.substring(i + 1, j ), "?"));
+                i = j ;
 
-                result.append(map.getOrDefault(value, "?"));
-                start = -1;
             }
-            else if (start == -1) {
+            else {
                 result.append(ch);
             }
+
+
         }
 
         return result.toString();
